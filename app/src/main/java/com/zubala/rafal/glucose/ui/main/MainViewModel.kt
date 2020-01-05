712ee0e56d954854e6a015a83777ad288781c11a
Rafał Zubala <rzubala@gmail.com>
2020-01-05 13:42:26 +0100

    sign in with google

--- app/src/main/java/com/zubala/rafal/glucose/ui/main/MainViewModel.kt
+++ app/src/main/java/com/zubala/rafal/glucose/ui/main/MainViewModel.kt
@@ -2,10 +2,20 @@ package com.zubala.rafal.glucose.ui.main
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+const val SHEET_URL = "https://docs.google.com/spreadsheets/d/1fLdEapcTUxaxoCN9YWW_LReRJJoQMbNR3bun4jQseDw/edit?usp=sharing"
 
 class MainViewModel : ViewModel() {
 
     fun onSubmit() {
-        Log.i("ViewModel", "Submit")
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                Log.i("ViewModel", "Submit")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/zubala/rafal/glucose/ui/signin/SigninFragment.kt
@@ -0,0 +1,76 @@
+package com.zubala.rafal.glucose.ui.signin
+
+import android.accounts.Account
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.*
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
+import com.google.android.gms.tasks.Task
+import com.zubala.rafal.glucose.R
+import com.zubala.rafal.glucose.databinding.SigninFragmentBinding
+
+class SigninFragment : Fragment() {
+
+    val RC_SIGN_IN: Int = 1
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    lateinit var mGoogleSignInOptions: GoogleSignInOptions
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: SigninFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.signin_fragment, container, false)
+
+        configureGoogleSignIn()
+
+        binding.signInButton.setOnClickListener {
+            val signInIntent: Intent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+        if (GoogleSignIn.getLastSignedInAccount(context) == null) {
+            startActivityForResult(mGoogleSignInClient.signInIntent, RC_SIGN_IN)
+        } else {
+            // use the already signed in account
+            val account = GoogleSignIn.getLastSignedInAccount(context)?.account
+            Log.i("SigninFragment", "already signed: ${account?.name}")
+        }
+
+
+        return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                //val account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)?.account
+                val account = task.getResult(ApiException::class.java)
+                Log.i("SigninFragment", "Signed: ${account?.toString()}")
+            } catch (e: ApiException) {
+                Log.w("SigninFragment", "signInResult: failed code=" + e.statusCode + ", reason: " + GoogleSignInStatusCodes.getStatusCodeString(e.statusCode), e)
+            }
+        }
+    }
+
+    private fun configureGoogleSignIn() {
+        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(Scope("https://www.googleapis.com/auth/spreadsheets"))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(context!!, mGoogleSignInOptions)
+    }
+}
