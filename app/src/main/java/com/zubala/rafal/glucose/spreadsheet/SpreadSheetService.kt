b0ab94aa8f8aa7b31fe685f26133e051cbdca2b2
Rafał Zubala <rzubala@gmail.com>
2020-01-06 18:14:15 +0100

    data with time

--- app/src/main/java/com/zubala/rafal/glucose/spreadsheet/SpreadSheetService.kt
+++ app/src/main/java/com/zubala/rafal/glucose/spreadsheet/SpreadSheetService.kt
@@ -1,6 +1,5 @@
 package com.zubala.rafal.glucose.spreadsheet
 
-import android.R.attr.data
 import android.util.Log
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import com.google.api.services.sheets.v4.Sheets
@@ -13,7 +12,7 @@ import java.io.IOException
 
 const val SPREADSHEET_ID = "1fLdEapcTUxaxoCN9YWW_LReRJJoQMbNR3bun4jQseDw"
 const val SHEET_NAME = "Arkusz1"
-const val VALUE_INPUT_OPTION = "RAW"
+const val VALUE_INPUT_OPTION = "USER_ENTERED"
 const val DATE_ROW_START = 3
 const val LIMIT_ROWS = 250
 
@@ -25,7 +24,7 @@ class SpreadSheetService {
 
         try {
             val result: ValueRange = sheets.spreadsheets().values()
-                .get(SPREADSHEET_ID, toRange("A", DATE_ROW_START, DATE_ROW_START + LIMIT_ROWS))
+                .get(SPREADSHEET_ID, toRange("A", "A", DATE_ROW_START, DATE_ROW_START + LIMIT_ROWS))
                 .execute()
             Log.i("SpreadSheetService", result.toPrettyString())
             var cnt = 0
@@ -45,10 +44,12 @@ class SpreadSheetService {
     }
 
     fun insertValue(sheets: Sheets, row: Int, data: String) {
+        val date = getCurrentDateTime()
+        val timeString = date.toString("hh:mm")
         try {
-            val values: MutableList<List<Any>> = MutableList(1) { MutableList(1) { data } }
+            val values: MutableList<List<Any>> = MutableList(1) { listOf(timeString, data) }
             val body: ValueRange = ValueRange().setValues(values)
-            val range = toRange("C", row, row)
+            val range = toRange("B", "C", row, row)
             val result: UpdateValuesResponse = sheets.spreadsheets().values().update(SPREADSHEET_ID, range, body)
                     .setValueInputOption(VALUE_INPUT_OPTION)
                     .execute()
@@ -60,26 +61,5 @@ class SpreadSheetService {
         }
     }
 
-    fun test(sheets: Sheets, data: String) {
-        try {
-            val values: MutableList<List<Any>> = mutableListOf()
-            repeat(10) {
-                values.add(listOf("a$it $data"))
-            }
-            val body: ValueRange = ValueRange().setValues(values)
-
-            val range = toRange("A", 1, 10)
-            val result: UpdateValuesResponse =
-                sheets.spreadsheets().values().update(SPREADSHEET_ID, range, body)
-                    .setValueInputOption(VALUE_INPUT_OPTION)
-                    .execute()
-            Log.i("SpreadSheetService", result.toPrettyString())
-        } catch (ex: UserRecoverableAuthIOException) {
-            Log.e("SpreadSheetService", "UserRecoverableAuthIOException", ex)
-        } catch (e: IOException) {
-            Log.e("SpreadSheetService", "failure to get spreadsheet: ", e)
-        }
-    }
-
-    private inline fun toRange(col: String, from: Int, to: Int): String = "'$SHEET_NAME'!$col$from:$col$to"
+    private inline fun toRange(col1: String, col2: String, from: Int, to: Int): String = "'$SHEET_NAME'!$col1$from:$col2$to"
 }
