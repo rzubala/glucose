775b269cc3cc1bc38f4e0bf84088cb67d372babe
Rafał Zubala <rzubala@gmail.com>
2020-01-13 12:44:29 +0100

    appcenter

--- app/src/main/java/com/zubala/rafal/glucose/MainActivity.kt
+++ app/src/main/java/com/zubala/rafal/glucose/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.zubala.rafal.glucose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.zubala.rafal.glucose.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.main_activity)
+
+        AppCenter.start(
+            application, "f24dc627-a7a3-4a51-8a6c-d67fc2b4452c",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
